15da63a2d8fbde713c6843de1166b054ef8f567f
serllo999 <97665855+serllo999@users.noreply.github.com>
2022-02-09 09:00:29 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,74 @@
+enum RadioMessage {
+    message1 = 49434
+}
+let numero = 0
+input.onGesture(Gesture.Shake, function () {
+    numero = randint(1, 8)
+    if (numero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (numero == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (numero == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (numero == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (numero == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (numero == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    } else if (numero == 7) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # # .
+            . # . # .
+            . . . . .
+            `)
+    } else if (numero == 8) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
